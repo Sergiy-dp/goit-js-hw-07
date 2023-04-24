6782aa9c3bb29745870c4d085d25448df5c14618
Sergiy-dp <sergeyeremenko16@gmail.com>
2023-04-24 23:15:50 +0300

    Create 02-lightbox.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+
+// Create and render gallery items
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`;
+
+  galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+
+  // Initialize SimpleLightbox
+
+const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
